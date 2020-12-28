692c61dbe8abab9b738a91b3d0cc92c2a9453a1b
Tomasz Reczek <reczet01@heiway.net>
2020-12-28 01:13:02 +0100

    Reorganization of scraping module. Creation of Scraper classes that will also handle connection to DB in the future. Scraping is already working but need some tweaks in concurrent speech scraping

--- src/data/scraping.py
+++ src/data/scraping.py
@@ -1,262 +1,263 @@
+"""
+This module contain scrapers that can extract data from the government website.
+"""
+
 import re
-import tempfile
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from pathlib import Path
 
 import bs4 as bs
-import pdftotext
 import requests
 from dateutil.parser import parse
 
-# TODO Znaleźć lepszy sposób na cięcie początku dokumentu
-# TODO Napisać wyrażenie regularne które będzie cięło stenogram na wypowiedzi posłów
+
+class Scraper:
+    """
+    Scraper base class
+    """
+
+    def __init__(self, government_n=9):
+        """
+        Creates instance of a Scraper
+        Args:
+            government_n (int): which government cadence should be scraped.
+                                Default is 9, which is cadence that ruled since 2019 to 2023
+        """
+        self.government_n = government_n
+        self.root_url = fr'https://www.sejm.gov.pl/sejm{self.government_n}.nsf/'
 
 
-def scrape_politician_speeches(government_n):
-    url = fr'https://www.sejm.gov.pl/sejm{government_n}.nsf/stenogramy.xsp'
-    url_to_specific_years = get_stenogram_year_links(url)
+class SpeechesScraper(Scraper):
+    """
+    Scraper that crawl through all politician speeches urls and extracts text from stenograms.
+    """
+    def __init__(self, government_n):
+        super().__init__(government_n)
 
-    all_pdfs = list()
-    for year_url in url_to_specific_years:
-        all_pdfs.extend(get_all_pdf_links_from_url(year_url))
+        self.speeches_url = self.root_url + r'/wypowiedzi.xsp'
 
-    for pdf_url in all_pdfs:
-        print(process_pdf_stenogram(pdf_url))
+        # Namespace
+        self.speeches = []
 
+    def scrape_politician_speeches(self):
+        """
+        Scraping all speeches and adding them to the speeches attribute
+        """
 
-def process_pdf_stenogram(pdf_url):
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self._scrape_all_speeches,
+                                       id=politician_id, name=politician_name, suffix=politician_suffix)
+                       for politician_id, politician_name, politician_suffix
+                       in self._speeches_per_politician_url(self.speeches_url + '?view=3')]
 
-    pdf_text_in_pages = extract_text_from_pdf(pdf_url)
-    pdf_text = "\n".join([parse_stenogram_page(page) for page in pdf_text_in_pages])
-    cleaned_text = clean_transcript_document(pdf_text)
-    return cleaned_text
+        for thread in as_completed(futures):
+            self.speeches += thread.result()
 
+        for politician_id, politician_name, politician_suffix in self._speeches_per_politician_url(self.speeches_url + '?view=3'):
+            self.speeches += self._scrape_all_speeches(id=politician_id, name=politician_name, suffix=politician_suffix)
 
-def get_stenogram_year_links(url):
-    web = requests.get(url)
-    soup = bs.BeautifulSoup(web.content, "html.parser")
-    links = [line.get('href') for line in soup.find_all("a")]
-    pattern = re.compile(r"\?rok=\d{4}")
-    return [url + pattern.match(link).group() for link in links if pattern.match(link)]
+    def _scrape_all_speeches(self, id, name, suffix):
+        speeches = []
+        for speech_url in self._iterate_through_speeches_in_politician_url(self.speeches_url + suffix):
+            speeches.append(
+                (id, name, self._extract_text_from_speech(self.root_url + speech_url))
+            )
+        return speeches
 
+    @staticmethod
+    def _extract_text_from_speech(url):
+        """
 
-def get_all_pdf_links_from_url(url):
-    web = requests.get(url)
-    soup = bs.BeautifulSoup(web.content, "html.parser")
-    all_links = [link.get('href') for link in soup.findAll('a')]
-    return [link for link in all_links if link[-3:] == "pdf"]
+        Args:
+            url:
 
+        Returns:
 
-def extract_text_from_pdf(pdf_url):
-    response = requests.get(pdf_url)
+        """
 
-    with tempfile.NamedTemporaryFile(suffix='.pdf', prefix="temp", delete=False) as file:
-        Path(file.name).write_bytes(response.content)
-        with open(file.name, "rb") as pdf:
-            pdf_text = pdftotext.PDF(pdf)
+        soup = bs.BeautifulSoup(requests.get(url).content, features="html.parser")
 
-    return pdf_text
+        # Cleaning function that will be used will looping through parts of speech
+        cleen_text = lambda speech_part: speech_part.get_text(strip='\xa0').replace("\r\n", "")
 
+        speech = " ".join(
+            [cleen_text(speech_part)
+             for speech_part in soup.find("div", {"class": "stenogram"}).findAll("p")[1:]])
 
-def parse_stenogram_page(page_text):
-    pattern = re.compile(r'\s?\s?(?P<LEFT>.*?)\s{3}\s?(?P<RIGHT>.*)')
+        return speech
 
-    left = list()
-    right = list()
-
-    omit_lines = ['KANCELARIA SEJMU: redakcja i skład – Sekretariat Posiedzeń Sejmu',
-                  'Informacja dla Sejmu i Senatu RP o udziale RP w pracach',
-                  '. . . . . . .',
-                  'TŁOCZONO Z POLECENIA MARSZAŁKA SEJMU',
-                  'PL ISSN',
-                  'Sprawozdanie Stenograficzne',
-                  ]
-
-    omit_regex = [r'z \d\d?. posiedzenia Sejmu Rzeczypospolitej Polskiej',
-                  r'posiedzenie Sejmu w dniu \d\d? listopada \d{4} r.',
-                  r'\s\s+\d+\s\s+']
-
-    for line in page_text.split("\r\n"):
-        if any([omit_line in line for omit_line in omit_lines]) or any(
-                [re.search(pattern, line) for pattern in omit_regex]):
-            continue
-        elif line[:5] == " " * 5:
-            right.append(line.lstrip(" "))
-        elif len(line) < 65:
-            left.append(line.strip(" "))
+    def _iterate_through_speeches_in_politician_url(self, url):
+        soup = bs.BeautifulSoup(requests.get(url).content, features="html.parser")
+        print(url)
+        pages = []
+        if not (page_navigation := soup.findAll("ul", {"class": "pagination"})):
+            pages.append(url)
         else:
+            for page_tag in page_navigation[0].findAll("li")[1:-1]:
+                pages.append(self.speeches_url + page_tag.findChild().get_attribute_list("href")[0].replace(" ", '%20'))
+
+        for page_url in pages:
+            soup = bs.BeautifulSoup(requests.get(page_url).content, features="html.parser")
+            table = soup.find('table', {'class': "table border-bottom lista-wyp"})
+
+            for row in table.findAll("tr"):
+                try:
+                    yield row.findAll("td")[-2].find("a").get_attribute_list("href")[0]
+                except IndexError:
+                    continue
+
+    @staticmethod
+    def _speeches_per_politician_url(url):
+        soup = bs.BeautifulSoup(requests.get(url).content, features="html.parser")
+        for politician in soup.find('ul', {'class': "category-list"}).find_all("li"):
+            for tag in politician:
+                if link := tag.get_attribute_list("href")[0]:
+                    politician_id = link[-3:]
+                    politician_name = tag.get_text()
+                    yield politician_id, politician_name, link
+
+
+class PoliticiansScraper(Scraper):
+
+    def __init__(self, government_n):
+        super().__init__(government_n)
+
+        self.politicians = []
+
+    def scrape_politicians(self):
+
+        padding = lambda n: str(n).rjust(3, "0")
+
+        last_politician_number = self._find_last_politician_number()
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self._scrape_single_politician,
+                                       url=self.root_url + rf'posel.xsp?id={padding(politician_number)}&type=A')
+                       for politician_number in range(1, last_politician_number + 1)]
+
+        self.politicians += [thread.result() for thread in as_completed(futures)]
+
+        # Scrape additional politicians that doesn't exist on the website
+        while True:
             try:
-                left_part, right_part = pattern.search(line).groupdict().values()
-                left.append(left_part.strip(" "))
-                right.append(right_part.strip(" "))
-            except AttributeError:
-                print('ParserError', len(line), line)
-
-    return "\n".join([*left, *right])
-
-
-def clean_transcript_document(document):
-
-    document = document.split("\n")
-
-    def cut_the_introduction(list_of_lines):
-        pattern = re.compile(r"^Marszałek:")
-        for ix, line in enumerate(document):
-            if pattern.search(line):
+                self.politicians.append(
+                    self._scrape_single_politician(
+                        self.root_url + rf'posel.xsp?id={padding(int(last_politician_number) + 1)}&type=A'))
+            except StopIteration:
                 break
-        return list_of_lines[ix:]
+            last_politician_number += 1
 
-    def cut_the_end(list_of_lines):
-        pattern = re.compile(r'^Porządek dzienny*\)')
-        for ix, line in enumerate(reversed(list_of_lines)):
-            if pattern.search(line):
+    def _find_last_politician_number(self):
+        web = requests.get(self.root_url + 'poslowie.xsp?type=A')
+        soup = bs.BeautifulSoup(web.content, 'html.parser')
+
+        all_links = [line.get_attribute_list("href") for line in soup.findAll("a")]
+
+        pattern = re.compile(r"posel\.xsp\?id=(?P<number>\d{3})")
+        for link in reversed(all_links):
+            if match := pattern.search(link[0]):
+                last_number = int(match.groupdict()['number'])
                 break
-        return list_of_lines[:-ix]
 
-    document = cut_the_introduction(document)
-    document = cut_the_end(document)
-    document = " ".join(document)
+        return last_number
 
-    # Regex replace patterns
-    regexes = \
-        [
-            (re.compile(r'\(.*?\)'), ""),  # parenthesis
-            (re.compile(r'(\s\s*)'), " "),  # excessive_spaces
-            (re.compile(r'(\w+)- '), r"\1")  # line breaking
-        ]
+    def _scrape_single_politician(self, url):
+        politician_info = dict()
+        web = requests.get(url)
+        soup = bs.BeautifulSoup(web.content, 'html.parser')
 
-    for pattern, repl in regexes:
-        document = re.sub(pattern, repl, document)
+        politician_info["name"] = soup.find(id="title_content").find("h1").get_text()
 
-    return document
+        if not politician_info["name"]:
+            raise StopIteration("URL doesn't contain any politician data.")
 
+        features = list()
+        values = list()
 
-def find_last_politician_number(url):
+        for section in ["partia", "cv"]:
+            for field in soup.find("div", {"class": section}).find("ul", {'class': "data"}):
+                features.append(field.find("p", {"class": "left"}).get_text()[:-1])  # Last sign is a ":"
+                values.append(field.find("p", {"class": "right"}).get_text())
 
-    web = requests.get(url + 'poslowie.xsp?type=A')
-    soup = bs.BeautifulSoup(web.content, 'html.parser')
+        feature_map = {
+            'Wybrany dnia': "election_date",
+            'Lista': "election_list",
+            'Okręg wyborczy': "election_area",
+            'Liczba głosów': "number_of_votes",
+            'Ślubowanie': "oath_date",
+            'Staż parlamentarny': "parliment_member",
+            'Klub/koło': "political_group",
+            'Data i miejsce urodzenia': "place_and_date_of_brith",
+            'Wykształcenie': 'education',
+            'Ukończona szkoła': 'school',
+            'Zawód': 'profession',
+            'Wygaśnięcie mandatu': 'resign_date',
+            'Wybrana dnia': "election_date",
+            'Tytuł/stopień naukowy': 'academic_degree'}
 
-    all_links = [line.get_attribute_list("href") for line in soup.findAll("a")]
+        features = map(lambda feature: feature_map.get(feature, feature), features)
 
-    pattern = re.compile(r"posel\.xsp\?id=(?P<number>\d{3})")
-    for link in reversed(all_links):
-        if match := pattern.search(link[0]):
-            last_number = int(match.groupdict()['number'])
-            break
+        politician_info.update(**dict(zip(features, values)))
 
-    return last_number
-
-
-def extract_all_politician_info(government_n=9):
-    url = rf'https://www.sejm.gov.pl/sejm{government_n}.nsf/'
-    padding = lambda n: str(n).rjust(3, "0")
-
-    last_politician_number = find_last_politician_number(url)
-    with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(scrape_politician_information,
-                                   url=url + rf'posel.xsp?id={padding(politician_number)}&type=A')
-                   for politician_number in range(1, last_politician_number+1)]
-
-    return [thread.result() for thread in as_completed(futures)]
-
-
-def scrape_politician_information(url):
-    politician_info = dict()
-    web = requests.get(url)
-    soup = bs.BeautifulSoup(web.content, 'html.parser')
-
-    politician_info["name"] = soup.find("title").get_text()
-
-    if not politician_info["name"]:
-        raise StopIteration("URL doesn't contain any politician data.")
-
-    features = []
-    values = []
-
-    looking_for_feature = True
-    for line in soup.findAll("p"):
-        if line.get_attribute_list("class") == ["left"] and looking_for_feature:
-            features.append(line.get_text().strip(":"))
-            looking_for_feature = False
-        elif line.get_attribute_list("class") == ["right"] and not looking_for_feature:
-            values.append(line.get_text().strip(":"))
-            looking_for_feature = True
-
-    feature_map = {
-        'Wybrany dnia': "election_date",
-        'Lista': "election_list",
-        'Okręg wyborczy': "election_area",
-        'Liczba głosów': "number_of_votes",
-        'Ślubowanie': "oath_date",
-        'Staż parlamentarny': "parliment_member",
-        'Klub/koło': "political_group",
-        'Data i miejsce urodzenia': "place_and_date_of_brith",
-        'Wykształcenie': 'education',
-        'Ukończona szkoła': 'school',
-        'Zawód': 'profession',
-        'Email': 'email',
-        'Wygaśnięcie mandatu': 'resign_date',
-        'Wybrana dnia': "election_date",
-        'Tytuł/stopień naukowy': 'academic_degree'}
-
-    features = map(lambda feature: feature_map.get(feature, feature), features)
-
-    politician_info.update(**dict(zip(features, values)))
-    try:
-        politician_info["email"] = soup.findAll("p")[-1].find("a").get("href")
-    except AttributeError:  # email is not available
-        pass
-
-    return clean_politician_data(politician_info)
-
-
-def clean_politician_data(politician_dict):
-    def parse_election_area(value):
-        nonlocal politician_dict
-
-        election_area_n, election_area = value.split("\xa0\xa0")
-        politician_dict['election_area_n'] = election_area_n
-        return election_area
-
-    def parse_place_and_date_of_birth(value):
-        nonlocal politician_dict
-
-        date_of_birth, place_of_birth = value.split(", ")
-        politician_dict['place_of_birth'] = place_of_birth
-        politician_dict['date_of_birth'] = parse(date_of_birth)
-
-    def assemble_email(value):
-
-        replacements = (
-            ("#", ""),
-            (" A T ", "@"),
-            (" D O T ", "."),
-            (" ", "")
-        )
-        for replacement in replacements:
-            value = value.replace(*replacement)
-
-        return value.lower()
-
-    transform = {
-        'election_date': lambda value: parse(value),
-        'election_area': parse_election_area,
-        'oath_date': lambda value: parse(value),
-        'parliment_member': lambda value: re.findall(r'([XVI]+)', value).extend("IX"),
-        'place_and_date_of_brith': parse_place_and_date_of_birth,
-        'email': assemble_email
-    }
-
-    for feature, value in list(politician_dict.items()):  # We are changing the dictionary while looping
         try:
-            politician_dict[feature] = transform[feature](value)
-        except KeyError:
-            continue
+            politician_info["email"] = soup.find(id="PoselEmail").next_sibling()[0]["href"]
+        except AttributeError:  # email is not available
+            pass
 
-    del politician_dict['place_and_date_of_brith']
+        return self._clean_politician_data(politician_info)
 
-    return politician_dict
+    @staticmethod
+    def _clean_politician_data(politician_dict):
+        def parse_election_area(value):
+            nonlocal politician_dict
+
+            election_area_n, election_area = value.split("\xa0\xa0")
+            politician_dict['election_area_n'] = election_area_n
+            return election_area
+
+        def parse_place_and_date_of_birth(value):
+            nonlocal politician_dict
+
+            date_of_birth, place_of_birth = value.split(", ")
+            politician_dict['place_of_birth'] = place_of_birth
+            politician_dict['date_of_birth'] = parse(date_of_birth)
+
+        def assemble_email(value):
+
+            replacements = (
+                ("#", ""),
+                (" A T ", "@"),
+                (" D O T ", "."),
+                (" ", "")
+            )
+            for replacement in replacements:
+                value = value.replace(*replacement)
+
+            return value.lower()
+
+        transform = {
+            'election_date': lambda value: parse(value),
+            'election_area': parse_election_area,
+            'oath_date': lambda value: parse(value),
+            'parliment_member': lambda value: re.findall(r'([XVI]+)', value).extend("IX") or [],
+            'place_and_date_of_brith': parse_place_and_date_of_birth,
+            'email': assemble_email
+        }
+
+        for feature, value in list(politician_dict.items()):  # We are changing the dictionary while looping
+            try:
+                politician_dict[feature] = transform[feature](value)
+            except KeyError:
+                continue
+
+        del politician_dict['place_and_date_of_brith']
+
+        return politician_dict
 
 
 if __name__ == '__main__':
-    scrape_politician_speeches(9)
+    # ps = PoliticiansScraper(government_n=9)
+    # ps.scrape_politicians()
+    # print(len(ps.politicians))
+
+    ss = SpeechesScraper(government_n=9)
+    ss.scrape_politician_speeches()
+    print(len(ss.speeches))
