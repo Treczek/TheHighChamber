668370352f3998620e5348d8fc680fd460f2e0bc
Tomasz Reczek <reczet01@heiway.net>
2021-01-06 21:59:56 +0100

    only_new parameter for scraping new speeches bugfixing swapping_name_with_surname deletion of local_backuping scraped data

--- src/mongo/utils/__init__.py
+++ src/mongo/utils/__init__.py
@@ -1,1 +1,5 @@
-from .utils import find_politician_by_name, create_speech_object, insert_speech_into_db, create_politician_object
+from .utils import (find_politician_by_name,
+                    create_speech_object,
+                    insert_speech_into_db,
+                    create_politician_object,
+                    get_last_speech_per_politician)
